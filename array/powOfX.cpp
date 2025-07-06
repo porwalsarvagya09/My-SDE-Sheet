d113619365141c0ad0997d68b641e30408348898
porwalsarvagya09 <porwalsarvagya09@gmail.com>
2025-07-06 10:21:50 +0530

    find-pow-n

---
+++ array/powOfX.cpp
@@ -0,0 +1,53 @@
+// leetcode - 50
+// pow(x,n)
+
+
+/*-------------- APPROACH -1 -----------------*/
+
+// T.C. : O(n)
+// S.C. : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+double myPow(double x, int n) {
+      double ans = 1.0;
+      for (int i = 0; i < n; i++) {
+        ans = ans * x;
+      }
+      return ans;
+}
+int main()
+{
+    cout<<myPow(2,10)<<endl;
+}
+
+
+/*-------------- OPTIMAL APPROACH- 2 -----------------*/
+
+// T.C. : O(log n)
+// S.C. : O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+double myPow(double x, int n) {
+  double ans = 1.0;
+  long long nn = n;
+  if (nn < 0) nn = -1 * nn;
+  while (nn) {
+    if (nn % 2) {
+      ans = ans * x;
+      nn = nn - 1;
+    } else {
+      x = x * x;
+      nn = nn / 2;
+    }
+  }
+  if (n < 0) ans = (double)(1.0) / (double)(ans);
+  return ans;
+}
+
+int main() {
+  cout << myPow(2, 10) << endl;
+}
