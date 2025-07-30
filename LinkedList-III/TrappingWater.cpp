c13947270975d03930ef331fc8e2cea899a87611
porwalsarvagya09 <porwalsarvagya09@gmail.com>
2025-07-30 17:25:16 +0530

    Add implementation for Trapping Rain Water

---
+++ LinkedList-III/TrappingWater.cpp
@@ -0,0 +1,123 @@
+// leetcode - 42
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int trap(vector < int > & arr) {
+  int n = arr.size();
+  int waterTrapped = 0;
+  for (int i = 0; i < n; i++) {
+    int j = i;
+    int leftMax = 0, rightMax = 0;
+    while (j >= 0) {
+      leftMax = max(leftMax, arr[j]);
+      j--;
+    }
+    j = i;
+    while (j < n) {
+      rightMax = max(rightMax, arr[j]);
+      j++;
+    }
+    waterTrapped += min(leftMax, rightMax) - arr[i];
+  }
+  return waterTrapped;
+}
+
+int main() {
+  vector < int > arr;
+  arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+  cout << "The water that can be trapped is " << trap(arr) << endl;
+}
+// Output: The water that can be trapped is 6
+
+// Time Complexity: O(N*N) as for each index we are calculating leftMax and rightMax so it is a nested loop.
+
+// Space Complexity: O(1).
+
+
+//--------------------------------------------------------------------
+
+// Better Approach
+
+// Approach: Take 2 array prefix and suffix array and precompute the 
+// leftMax and rightMax for each index beforehand. Then use the formula 
+// min(prefix[I], suffix[i])-arr[i] to compute water trapped at each index.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int trap(vector < int > & arr) {
+  int n = arr.size();
+  int prefix[n], suffix[n];
+  prefix[0] = arr[0];
+  for (int i = 1; i < n; i++) {
+    prefix[i] = max(prefix[i - 1], arr[i]);
+  }
+  suffix[n - 1] = arr[n - 1];
+  for (int i = n - 2; i >= 0; i--) {
+    suffix[i] = max(suffix[i + 1], arr[i]);
+  }
+  int waterTrapped = 0;
+  for (int i = 0; i < n; i++) {
+    waterTrapped += min(prefix[i], suffix[i]) - arr[i];
+  }
+  return waterTrapped;
+}
+
+int main() {
+  vector < int > arr;
+  arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+  cout << "The water that can be trapped is " << trap(arr) << endl;
+}
+// Output: The water that can be trapped is 6
+
+// Time Complexity: O(3*N) as we are traversing through the array only once. And O(2*N) for computing prefix and suffix array.
+
+// Space Complexity: O(N)+O(N) for prefix and suffix arrays.
+
+
+
+
+//----------------------------------------------------------------------------
+
+
+// Optimal Solution(Two pointer approach)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int trap(vector < int > & height) {
+  int n = height.size();
+  int left = 0, right = n - 1;
+  int res = 0;
+  int maxLeft = 0, maxRight = 0;
+  while (left <= right) {
+    if (height[left] <= height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        res += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        res += maxRight - height[right];
+      }
+      right--;
+    }
+  }
+  return res;
+}
+
+int main() {
+  vector < int > arr;
+  arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+  cout << "The water that can be trapped is " << trap(arr) << endl;
+}
+// Output: The water that can be trapped is 6
+
+// Time Complexity: O(N) because we are using 2 pointer approach.
+
+// Space Complexity: O(1) because we are not using anything extra.
